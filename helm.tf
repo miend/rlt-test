ec72aab2ae545d7b32f5a4d9ba8a9bf995b4336b
Michael Endsley <michael.endsley@pearson.com>
2020-07-23 22:10:42 -0500

    Have helm_release wait for appropriate IAM policies to be fetched for pulling images from our GCR, or it may deploy broken pods that are missing appropriate permissions.

--- helm.tf
+++ helm.tf
@@ -1,5 +1,5 @@
 resource "google_compute_address" "ingress" {
-  name    = "${var.name}-ingress-static-ip"
+  name    = "${var.cluster_name}-ingress-static-ip"
   project = var.project
   region  = var.region
 }
@@ -21,6 +21,10 @@ resource  "helm_release" "nginx-ingress" {
 
 }
 
+# There are no TLS certs configured, and those would definitely be needed for production.
+# I would include steps to have Helm install cert-manager, then provision certs similar to:
+# https://medium.com/google-cloud/https-with-cert-manager-on-gke-49a70985d99b
+
 resource "helm_release" "rlt-demo" {
   for_each = var.namespaces
   name             = "rlt-demo"
@@ -38,4 +42,24 @@ resource "helm_release" "rlt-demo" {
     name  = "ingress.hosts[0]"
     value = "${each.value["host_prefix"]}${var.host}"
   }
+
+  depends_on = [
+    google_storage_bucket_iam_member.member,
+  ]
 }
+
+# Deploy DNS records after ingress is configured
+#resource "google_dns_managed_zone" "default" {
+#name     = "default-zone"
+#dns_name = "rlt-demo.exitprompt.com."
+#}
+#
+#For each namespace's host_prefix value, if not empty, create a record
+#resource "google_dns_record_set" "a" {
+#  name         = "host_prefix.${google_dns_managed_zone.default.dns_name}"
+#  managed_zone = google_dns_managed_zone.default.name
+#  type         = "A"
+#  ttl          = 300
+#
+#  rrdatas = ["${google_compute_address.ingress.address}"]
+#}
