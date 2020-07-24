2ae38b38d4f8eeb12b3ae446393766e6bcf44ec5
Michael Endsley <michael.endsley@pearson.com>
2020-07-23 23:39:17 -0500

    Move terraform to providers file to consolidate all version requirements. Include a reference to how GCS would be used as backend to store state remotely instead of including it in the repository.

--- providers.tf
+++ providers.tf
@@ -1,3 +1,9 @@
+terraform {
+  required_version = ">= 0.12.7"
+# Configure state storage to GCS backend here, as:
+# https://www.terraform.io/docs/backends/types/gcs.html
+}
+
 provider "google" {
   version = "~> 3.31.0"
   project = var.project
